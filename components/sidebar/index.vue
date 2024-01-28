2f1ffd2a4698bab67eaf6918492c5a36f7f6cfca
poojan-bhalodiya <poojanbhalodiya02@gamil.com>
2024-01-29 01:24:55 +0530

    image static

--- components/sidebar/index.vue
+++ components/sidebar/index.vue
@@ -1,8 +1,13 @@
 <template>
   <div class="bg-primary w-1/5 h-screen text-white p-2">
-    Header
+    <div class="">
+      <div>Header</div>
+      <div>Center</div>
+      <div>Social Profile</div>
+      <div>Version : {{ version }}</div>
+    </div>
   </div>
 </template>
 <script setup>
-
+import { version } from "@/package.json";
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -17,7 +17,7 @@ useHead({
     },
     {
       property: "image",
-      content: "https://poojan-bhalodiya.vercel.app/images/profile-1.jpg",
+      content: "https://poojan-bhalodiya.vercel.app/profile-1.jpg",
     },
     { hid: "og:type", property: "og:type", content: "website" },
     {
@@ -34,7 +34,7 @@ useHead({
     },
     {
       property: "og:image",
-      content: "https://poojan-bhalodiya.vercel.app/images/profile-1.jpg",
+      content: "https://poojan-bhalodiya.vercel.app/profile-1.jpg",
     },
     {
       name: "og:keywords",
