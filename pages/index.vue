cbdc7aab25e28e2b5693f406b23cb256c50ea169
poojan-bhalodiya <poojanbhalodiya02@gamil.com>
2024-01-29 00:13:11 +0530

    SEO setup

---
+++ pages/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>Page Index</div>
+  <!-- <img src="public/profile-pic.jpg" /> -->
+</template>
+<script setup>
+useHead({
+  title: "Poojan Bhalodiya",
+  meta: [
+    { property: "og:site_name", content: "Poojan Bhalodiya" },
+    {
+      name: "description",
+      content: `My Personal Portfolio`,
+    },
+    {
+      name: "keywords",
+      content: `poojan patel, Poojan Bhalodiya, Ahmedabad, Backend Developer`,
+    },
+    {
+      property: "image",
+      content: "public/profile-pic.jpg",
+    },
+    { hid: "og:type", property: "og:type", content: "website" },
+    {
+      property: "og:url",
+      content: `https://poojan-bhalodiya.vercel.app/`,
+    },
+    {
+      property: "og:title",
+      content:
+        `Poojan Bhalodiya`,
+    },
+    {
+      property: "og:description",
+      content: `My Personal Portfolio`,
+    },
+    {
+      property: "og:image",
+      content: "public/profile-pic.jpg",
+    },
+    {
+      name: "og:keywords",
+      content: `poojan patel, Poojan Bhalodiya, Ahmedabad, Backend Developer`,
+    },
+  ],
+});
+</script>
