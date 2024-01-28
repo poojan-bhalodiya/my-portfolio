81fe480aa31fb64b6a1180e97ad2839d4f82fca7
poojan-bhalodiya <poojanbhalodiya02@gamil.com>
2024-01-29 00:04:36 +0530

    init modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  modules: ["@vueuse/nuxt", "@nuxtjs/tailwindcss", "nuxt-icon", "nuxt-gtag"],
+  gtag: {
+    id: "G-YHWG5TVK7Z",
+  },
+});
