5ee673a9ba623461d30bce16429961fb0e6dc5ec
poojan-bhalodiya <poojanbhalodiya02@gamil.com>
2024-01-30 23:24:40 +0530

    changes

--- components/sidebar/index.vue
+++ components/sidebar/index.vue
@@ -1,20 +1,39 @@
 <template>
-  <div class="bg-primary w-1/5 h-screen text-white p-2">
-    <div class="">
+  <div class="bg-primary w-1/5 min-w-72 text-white">
+    <div class="h-full flex flex-col">
       <!-- Branding -->
-      <div>Header</div>
-
-      <!-- Navigations -->
-      <div></div>
-
+      <div
+        class="py-12 px-4 text-4xl font-semibold text-center border-b border-yellow-600"
+      >
+        <span class="text-yellow-600">P</span>oojan Bhalodiya
+      </div>
+      <div class="items-center h-full flex justify-center flex-col">
+        <!-- Navigations -->
+        <div class="py-4 border-b border-yellow-600 px-4">Link</div>
+      </div>
       <!-- Social Profile -->
-      <div>Social Profile</div>
-
+      <div class="py-4 border-b border-yellow-600 px-4 flex items-center">
+        <button></button>
+      </div>
       <!-- Application Version -->
-      <div>Version : {{ version }}</div>
+      <div class="border-t border-yellow-600 px-4 py-4 text-center">
+        Version : {{ version }}
+      </div>
     </div>
   </div>
 </template>
 <script setup>
 import { version } from "@/package.json";
+const socialLinks = [
+  {
+    icon: "",
+    name: "linktree",
+    link: "",
+  },
+  {
+    icon: "",
+    name: "linktree",
+    link: "",
+  },
+];
 </script>
