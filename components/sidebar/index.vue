1c878e78159988bd55135fd0595ff5ca4d29c77f
poojan-bhalodiya <poojanbhalodiya02@gamil.com>
2024-02-02 00:20:27 +0530

    feed seo

--- components/sidebar/index.vue
+++ components/sidebar/index.vue
@@ -14,8 +14,8 @@
       <div
         class="py-4 md:py-12 px-4 text-2xl md:text-4xl font-semibold text-center border-b border-yellow-600"
       >
-        Test App
-        <!-- <span class="text-yellow-600">P</span>oojan Bhalodiya -->
+        <!-- Test App -->
+        <span class="text-yellow-600">P</span>oojan Bhalodiya
       </div>
       <div
         class="items-center h-full flex justify-center flex-col"
--- data/blogs/detail.js
+++ data/blogs/detail.js
@@ -1,22 +1,57 @@
 const blogDetail = [
   {
-    banner: "https://res.cloudinary.com/dhntmsget/image/upload/v1706768266/blog-demo/2560X250.png",
-    thumbnail: "https://res.cloudinary.com/dhntmsget/image/upload/v1706768200/blog-demo/250X250.png",
+    banner:
+      "https://res.cloudinary.com/dhntmsget/image/upload/v1706768266/blog-demo/2560X250.png",
+    thumbnail:
+      "https://res.cloudinary.com/dhntmsget/image/upload/v1706768200/blog-demo/250X250.png",
     title: "Firebase database configuration steps",
-    description: "This is a firebase configuration strp by step explanation. you can folow the step and complete the config your firebase.",
+    description:
+      "This is a firebase configuration strp by step explanation. you can folow the step and complete the config your firebase.",
     date: "2 Sep 2023",
     tags: ["firebase"],
     slug: "firebase-configuration",
     meta: {
       title: "Firebase database configuration steps",
-      description: "This is a firebase configuration strp by step explanation. you can folow the step and complete the config your firebase.",
-      image: "https://res.cloudinary.com/dhntmsget/image/upload/v1706768200/blog-demo/250X250.png",
+      description:
+        "This is a firebase configuration strp by step explanation. you can folow the step and complete the config your firebase.",
+      image:
+        "https://res.cloudinary.com/dhntmsget/image/upload/v1706768200/blog-demo/250X250.png",
       url: "https://poojan-bhalodiya.vercel.app/blog/firebase-configuration",
-      keywords: ["firebase", "connect firebase","firestore","realtimedatabase","databaase"],
+      keywords: [
+        "firebase",
+        "connect firebase",
+        "firestore",
+        "realtimedatabase",
+        "databaase",
+      ],
     },
   },
-
-  
+  {
+    banner:
+      "https://res.cloudinary.com/dhntmsget/image/upload/v1706768266/blog-demo/2560X250.png",
+    thumbnail:
+      "https://res.cloudinary.com/dhntmsget/image/upload/v1706768200/blog-demo/250X250.png",
+    title: "How to set up a simple server using Node.js and Express",
+    description:
+      "this blog is a setup for a basic server using node.js and Express.",
+    date: "2 Feb 2024",
+    tags: ["Node.js", "Basic Node server", "Express.js", "Node.js + Express"],
+    slug: "simple-server-using-nodejs-express",
+    meta: {
+      title: "How to set up a simple server using Node.js and Express",
+      description:
+        "this blog is a setup for a basic server using node.js and Express.",
+      image:
+        "https://res.cloudinary.com/dhntmsget/image/upload/v1706768200/blog-demo/250X250.png",
+      url: "https://poojan-bhalodiya.vercel.app/blog/simple-server-using-nodejs-express",
+      keywords: [
+        "Node.js",
+        "Basic Node server",
+        "Express.js",
+        "Node.js + Express",
+      ],
+    },
+  },
 ];
 
 export default blogDetail;
